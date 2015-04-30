cc37af1ea9f10cfb5ab6df3d0f880652132a58c3
Carlos Asmat <casmat@aldebaran-robotics.com>
2015-04-30 13:06:28 -0400

    added support for reading analog pins, renamed fucntions for clarity

--- ALDuino/lib/alduino.py
+++ ALDuino/lib/alduino.py
@@ -2,9 +2,10 @@
 NAOqi service that controls an Arduino board runnning the StandardFirmata
 sketch.
 """
-
+from time import sleep
 from butane.fuel import Fuel
 from pyfirmata import Arduino
+from pyfirmata import util
 from serial import SerialException
 
 
@@ -14,11 +15,14 @@ class ALDuino(Fuel):
     def __init__(self, session, path):
         super(ALDuino, self).__init__(session, path)
         self.board = None
+        self.iter = None
 
     def connect_board(self, port):
         """Connect board in the given port."""
         try:
             self.board = Arduino(port)
+            self.iter = util.Iterator(self.board)
+            self.iter.start()
         except SerialException:
             return False
         return True
@@ -33,8 +37,11 @@ class ALDuino(Fuel):
                 break
         return success
 
-    def set_pin(self, pin_no, value):
-        """Set the digital pin value"""
+    def set_digital(self, pin_no, value):
+        """Set the digital pin value.
+        :param pin_no: integer pointing to the pin number.
+        :param value: 0 or 1 corresponding to LOW and HIGH respectively.
+        """
 
         pin_no = int(pin_no)
         value = int(value)
@@ -44,3 +51,22 @@ class ALDuino(Fuel):
         else:
             self.board.digital[pin_no].write(0)
             return True
+
+    def read_digital(self, pin_no):
+        """Read digital pin state.
+        :param pin_no: integer pointing to the pin number.
+        :returns: 0 or 1"""
+        pin_no = int(pin_no)
+        return self.board.digital[pin_no].read()
+
+    def read_analog(self, pin_no):
+        """Read analog pin.
+        :param pin_no: integer pointing to the pin number.
+        :returns: float between 0 and 1 included.
+        """
+        pin_no = int(pin_no)
+        self.board.analog[pin_no].enable_reporting()
+        sleep(0.01)
+        val = self.board.analog[pin_no].read()
+        self.board.analog[pin_no].enable_reporting()
+        return val
